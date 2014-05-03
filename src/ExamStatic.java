28c8d9fb63fbb3f28be592dc9a622547e9649b6e
Diamur <neelovdima@gmail.com>
2014-05-03 23:02:55 +0400

    Initial commit

---
+++ src/ExamStatic.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Diamur on 03.05.2014.
+ */
+public class ExamStatic {
+
+    public static int a =7;
+    public static String get(){
+        return "Приветики";
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Diamur on 03.05.2014.
+ */
+public class Main {
+    
+    public static void main(String... args){
+
+    }
+}
